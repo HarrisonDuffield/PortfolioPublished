79ea9cb9fbd2617c49a40411c5d196284ae2d2b5
Harrison Duffield <46687573+HarrisonDuffield@users.noreply.github.com>
2020-09-12 18:15:01 +0100

    Just coloring

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("I Know this uses JQuery and I appreciate it's not 2014 anymore and a bit overkill, but it works well enough for this.") 
+console.log("This site serves a temporary portfolio. Yes, it has been hastily put together.")
 var CurrentlyActivePage = "Home"
 console.log("This site was written rather quickly over the summer of 2020. My main site is under delicate construction, this merely servers as a stopgap.")
 document.getElementById("JSWarningMessage").style.display = "none";
@@ -6,26 +6,19 @@ document.getElementById("CurrentWebAddress").innerHTML = window.location.hostnam
 document.getElementById("BigDiv").style.display="none";
 document.getElementById("ExitCross").style.display="none";
 
+var WhatIsLeftBox = "Media" // tracks the boxes
 
 
 function scrollFunction() {
-    document.getElementById("HeaderDiv").style.padding = "5px 0px";
-    document.getElementById("Header").style.fontSize ="1rem";
     return;
-  if (document.body.scrollTop > 95 || document.documentElement.scrollTop > 95) {
-    document.getElementById("HeaderDiv").style.padding = "5px 0px";
-    document.getElementById("Header").style.fontSize ="1rem";
-  } else {
-    document.getElementById("HeaderDiv").style.padding = "10px 0px";
-    document.getElementById("Header").style.fontSize = "3rem";
-  }
+ 
 }
 
 var cards = $(".card");
 console.log(cards.length)
 WelcomeText();
 var WelcomeTracker = 0;
-for(var i = 0; i < cards.length; i++){
+for(var i = 0; i <= cards.length; i++){
     var target = Math.floor(Math.random() * cards.length -1) + 1;
     var target2 = Math.floor(Math.random() * cards.length -1) +1;
     cards.eq(target).before(cards.eq(target2));
@@ -56,21 +49,7 @@ function AddToWelcomeText(CharToAdd,counter){
 
 
 function WelcomeText(){
-    var TextToDisplay = ["Hello","Welcome","Have a Browse of my projects"];
-    var app = document.getElementById("SubheadingText");
-    var typewriter = new Typewriter(app, {
-        loop: true
-    });
-    
-    typewriter.typeString('student')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString('icon.')
-        .pauseFor(2500)
-        .deleteAll()
-        .typeString("majestic.")
-        .pauseFor(2500)
-        .start();
+    return;
 }
 function HideDivs(ElementToSave){//hide the non active div
     for(var i = 0; i < cards.length; i++){
@@ -86,17 +65,47 @@ function HideDivs(ElementToSave){//hide the non active div
 
 
 
+function ExpandOtherWork(){
+    HideDivs();
+    CurrentlyActivePage = "OtherWork"
+    var LinksToWork = ["news.bbc.co.uk"]
+    var DisplayName = ["bbcnews"]
+    var TextParagraphs = ["Here is a small but hopefully growing showvase of other bits of work I've completed recently"]
+    NonStandardExpandActiveDiv("Other Work");
+    for (var i=0;i<TextParagraphs.length;i++){
+        LeftAddContentText(TextParagraphs[i]);
+    }
+    for (var i=0;i<LinksToWork.length;i++){
+        AddButtonLink(LinksToWork[i],DisplayName[i]);
+    } 
+    
+}
 
 function ExpandSpotify(){//original plan was to have this all generate dynamically from json files, but CORS requests will stop that when hosting on github pages
     HideDivs();
     CurrentlyActivePage = "Spotify"
-    var TextParagraphs = ["Spotify Group Bangers is a system devised by msyelf with assistance from Daniel Perks , to curate spotify playlists together as a group. It is hosted on free tier heroku(so load times do slow down frequently), and is built on a flask backend."]
+    var TextParagraphs = ["My Spotify playist utility named 'Spotify Group Bangers' is a system devised by msyelf with assistance from Daniel Perks , to curate spotify playlists together as a group. It is hosted on free tier heroku(so load times do slow down frequently), and is built on a flask backend."]
     HeaderImage = 'Assets\\Spotify\\SpotifyDemo.png';
     ExapndActiveDiv(HeaderImage,"Spotify Group Bangers");
     for (var i=0;i<TextParagraphs.length;i++){
         AddContentText(TextParagraphs[i]);
     } 
-    AddButtonLink("http://spotify.harrisonduffield.com","Give It A Go")
+    AddButtonLink("http://spotify.harrisonduffield.com","Give It A Go");
+    AddButtonLink("https://github.com/HarrisonDuffield/SpotifyGroupPlaylist","View the Repository");
+}
+
+
+function ExpandMonocl(){
+    HideDivs();
+    CurrentlyActivePage = "Monocl"
+    var TextParagraphs = ["Monocl was a rudimentary undertaking of a web based langugae education service that would be able to attempt to improve its answers based over time to track changes in language. It allowed for the communtiy of users to submit questions and answers, and gave more trust to certain users questions and answers than others to moderate the system whilst allowing for these users (Teachers) to set homework easily etc." ,"Undertaken over a period of 6 months,starting in January of 2019, it recieved full marks when submitted.","Monocl was the first project made of those listed upon here, and really the first of any actual value I've ever done. It is by far the most well documented of any I have done since.", "Whilst images of the UI of Monocl do exist(and are included in the documentation), they are very poorly designed with the project being very backend heavy.","The documentation and repositories are linked below, Take a look."]
+    NonStandardExpandActiveDiv("Monocl");
+    for (var i=0;i<TextParagraphs.length;i++){
+        LeftAddContentText(TextParagraphs[i]);
+    } 
+    AddButtonLink("https://github.com/HarrisonDuffield/Monocl","Front End Repository")
+    AddButtonLink("https://github.com/HarrisonDuffield/MonoclBackend","Back End Repository")
+    AddButtonLink("https://github.com/HarrisonDuffield/MonoclBackend/blob/master/README.md","Documentation")
 }
 
 function ExpandPagoda(){
@@ -115,7 +124,7 @@ function ExpandPagoda(){
 function ExpandCircle(){//original plan was to have this all generate dynamically from json files, but CORS requests will stop that when hosting on github pages
     CurrentlyActivePage = "Circle"
     HideDivs();
-    var TextParagraphs = ["It may seem long ago now, but at the start of the pandemic the UK(and for that matter the US etc) was in the midst of a panic buying problem,culminating in toilet roll becoming scarce. To combat this , and to maintain social distancing the major UK supermarkets devised a system wehere people would have to queue 2m apart around their car parks. We didn't think this was an adequate enough solution,in particular with the stories of health workers not being able to queue, so we devised a queue booking system for supermarkets","The Circle is a project created for the HackQuarantine with Daniel Perks","Please contact me to view the private repostiory"]
+    var TextParagraphs = ["It may seem long ago now, but at the start of the pandemic the UK(and elsewhere) was in the midst of a panic buying problem,culminating in toilet roll becoming scarce. To combat this , and to maintain social distancing the major UK supermarkets devised a system where people would have to queue 2m apart around their car parks. We didn't think this was an adequate enough solution,in particular with the stories of the queuing system not really being flexible around timings,so we devised a queue booking system for supermarkets. This would also have had the benefit of tracking the people who were present at any time in the shop, to easily trace contacts in case of infection","The Circle is a project created for the HackQuarantine with Daniel Perks","Please contact me to view the private repostiory"]
     HeaderImage = 'http://img.youtube.com/vi/dUIdQCkoBqY/hqdefault.jpg'
     ExapndActiveDiv(HeaderImage,"The Circle");
     for (var i=0;i<TextParagraphs.length;i++){
@@ -133,11 +142,16 @@ function LinkedInHandler(){
 function PortfolioHandler(){
     CurrentlyActivePage = "Portfolio"
     HideDivs();
-    ExapndActiveDiv("","Portfolio")
-    var TextParagraphs = ["Welcome to my current 'portfolio'. It contains the highlights of recent projects I have worked on , aswell as some of the larger pieces of work undertaken as part of my studies.","My main portfolio is under construction(albeit slow), however I intend to make regular improvements to this site just to keep it up to date and fresh.","Keep your eye out, and take a look around","Like what you see? Say hello."]
+    HeaderImage = "https://avatars1.githubusercontent.com/u/46687573?s=460&u=85245301c86579b24eaadc5c553fd169716f36f4&v=4"
+    ExapndActiveDiv(HeaderImage,"About")
+    var LastUpdated = "This site was last updated in September 2020";
+    var TextParagraphs = ["Hi. I am a 19 years old and studying towards a Computer Science BSc at Coventry University in the United Kingdom." ,"Welcome to my current 'portfolio'. It contains the highlights of recent projects I have worked on , aswell as some of the larger pieces of work undertaken as part of my studies.","My main portfolio is under careful construction, however I intend to make regular improvements to this site just to keep it up to date and fresh.","Keep your eye out, and take a look around","Like what you see? Say hello.",LastUpdated]
     for (var i=0;i<TextParagraphs.length;i++){
         AddContentText(TextParagraphs[i]);
     } 
+
+    
+    
     AddButtonLink("https://www.linkedin.com/in/harrison-duffield-70615b196/","LinkedIn")
     AddButtonLink("https://github.com/HarrisonDuffield","Github")
    // AddIFrame(window.href.location,"Meta Portolio");
@@ -171,20 +185,45 @@ function ExitHomeMenu(){
         case "Spotify":
             ExpandSpotify();
             break;
+        case "OtherWork":
+            ExpandOtherWork();
+            break;
         case "Pagoda":
             ExpandPagoda();
+            break;
+        case "Portfolio":
+            PortfolioHandler();
             break;
         default:
             ExpandSpotify();
             break;
     }
 }
+
+function LeftRightSwitch(){
+    var leftBox = document.getElementById("MediaContentBox")
+    var RightBox = document.getElementById("TextContentBox")
+    initialLeftBoxId = LeftBox.id;
+    initialRightBoxId = RightBox.id;
+    LeftBox.id = initialRightBoxId;
+    RightBox.id = initialLeftBoxId;
+
+}
+
 function ExapndActiveDiv(HeaderImage,HeaderTitleContent){//clicked on div exapnds to full screen,picutre tops the article and dispaperas as scroll down, h1 header - back button to main page
 //comme une parralax?
-    ClearMainDiv()
+    ClearMainDiv();
     document.getElementById("ExitCross").style.display="none";
     document.getElementById("BigDiv").style.display="block";
-    document.getElementById("headerImage").src = HeaderImage;
+    document.getElementById("AlternateTextContent").style.display="none";
+    if(HeaderImage != "Blank"){
+        document.getElementById("headerImage").style.display = "";
+        document.getElementById("headerImage").src = HeaderImage;
+    }
+    else{
+        document.getElementById("headerImage").style.display="none";
+    }
+    
     document.getElementById("Header").innerHTML = HeaderTitleContent;
     
     var headerTitle = document.getElementById("HeaderDiv");
@@ -205,6 +244,33 @@ function ExapndActiveDiv(HeaderImage,HeaderTitleContent){//clicked on div exapnd
 
 }
 
+
+
+function NonStandardExpandActiveDiv(HeaderTitleContent){//Same But for using the left div instead of right div i.e specialist content
+    ClearMainDiv();
+    document.getElementById("ExitCross").style.display="none";
+    document.getElementById("BigDiv").style.display="block";
+    document.getElementById("AlternateTextContent").style.display="";
+    document.getElementById("headerImage").style.display="none";
+    document.getElementById("Header").innerHTML = HeaderTitleContent;
+    
+    var headerTitle = document.getElementById("HeaderDiv");
+    var stickyTitle = headerTitle.offsetTop;
+    window.onscroll = function(){
+        scrollFunction();
+        if (window.pageYOffset> stickyTitle) {
+           headerTitle.classList.add("sticky");
+           console.log("Sticky Set")
+           
+          }
+         else {
+            headerTitle.classList.remove("sticky");
+            console.log("Sticky Unset")
+          }
+
+    }
+}
+
 //function AddIFrame(Source,Title){
   //  var node = document.createElement("iframe");
     //var textnode = document.
@@ -215,10 +281,29 @@ function AddContentText(TextToAdd){
     var textnode = document.createTextNode(TextToAdd);
     node.appendChild(textnode);
     document.getElementById("TextContent").appendChild(node);
-    var node = document.createElement("br");//add a break
-    document.getElementById("TextContent").appendChild(node);
+   // var node = document.createElement("br");//add a break
+    //document.getElementById("TextContent").appendChild(node);
 
 }
+
+
+function LeftAddContentText(TextToAdd){
+    var node = document.createElement("p");
+    var textnode = document.createTextNode(TextToAdd);
+    node.appendChild(textnode);
+    document.getElementById("AlternateTextContent").appendChild(node);
+}
+
+function LeftAddHrefLinks(Destination,TextContent){
+    var node = document.createElement("button");
+    node.innerHTML = innerHTMLName
+    node.onclick =function(){
+        window.location.href = Destination;
+    }
+    node.classList.add("ButtonLink")
+    document.getElementById("AlternateTextContent").appendChild(node);
+
+};
 
 function AddButtonLink(Destination,innerHTMLName){
     console.log(Destination);
