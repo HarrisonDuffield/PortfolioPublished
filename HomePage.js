79d9098e96c904c5782af8c5da94bc1438c45d78
Harrison Duffield <46687573+HarrisonDuffield@users.noreply.github.com>
2020-09-27 18:37:45 +0100

    Fixed Links - Cut down on JS - JAmes contrast advice

--- HomePage.js
+++ HomePage.js
@@ -1,17 +1,16 @@
 document.getElementById("CurrentWebAddress").innerHTML = window.location.hostname;
-document.getElementById("BigDiv").style.display="none";
-document.getElementById("ExitCross").style.display="none";
-AddToHistory("Harrison Duffield","");
+
+
 var cards = $(".card");
 console.log(cards.length)
 WelcomeText();
 var WelcomeTracker = 0;
-for(var i = 0; i <= cards.length; i++){
+/* for(var i = 0; i <= cards.length; i++){
     var target = Math.floor(Math.random() * cards.length -1) + 1;
     var target2 = Math.floor(Math.random() * cards.length -1) +1;
     //cards.eq(target).before(cards.eq(target2));
 }
-
+ */
 
 function HideDivs(ElementToSave){//hide the non active div
     for(var i = 0; i < cards.length; i++){
